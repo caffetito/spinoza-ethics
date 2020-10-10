0d780b37f7370882ebf13e6bdb09495e7f4aaf10
caffetito <caffetito@gmail.com>
2020-10-10 13:41:23 +0200

    further developments

--- id.js
+++ id.js
@@ -1,19 +1,36 @@
-// function getUniqueKeyForNode (targetNode) {
-//     const pieces = ['el'];
-//     let node = targetNode;
+//////add ids to nodes
 
-//     while (node && node.parentNode) {
-//         pieces.push(Array.prototype.indexOf.call(node.parentNode.childNodes, node));
-//         node = node.parentNode
-//     }
-
-//     return pieces.reverse().join('-');
-// }
-
-console.log("it works!")
-
-var elements = document.body.getElementsByTagName('*');
-
+// var elements = document.body.getElementsByTagName('*');
 // Set their ids
-for (var i = 0; i < elements.length; i++)
-    elements[i].id = 'id-' + i;
+// for (var i = 0; i < elements.length; i++)
+//     elements[i].id = 'id-' + i;
+
+
+
+//////tooltip 
+function showTooltip(event) {
+
+    const target = event.target
+    target.previousElementSibling.classList.toggle('hidden')
+}
+
+function hideTooltip(event) {
+    const target = event.target
+    target.previousElementSibling.classList.toggle('hidden')
+    
+}
+
+const tooltips = document.querySelectorAll(".tooltip");
+
+tooltips.forEach(tooltip => {
+    const id = tooltip.dataset.reference
+    const referenceText = document.getElementById(id).innerText    
+    const tooltipSpan = document.createElement('span')
+    const tooltipText = document.createTextNode(referenceText)
+    tooltipSpan.appendChild(tooltipText)
+    tooltipSpan.classList.add('hidden' , 'tooltip-box')
+
+tooltip.parentNode.insertBefore(tooltipSpan, tooltip)
+    tooltip.addEventListener("mouseover", showTooltip)
+    tooltip.addEventListener('mouseout', hideTooltip)
+})
